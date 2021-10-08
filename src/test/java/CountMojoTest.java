ce9aedf3a91f3921eff44e001ff22669284262b0
Zhaofeng Zhou <aaaa159557190@gmail.com>
2021-10-08 21:24:39 +0800

    Add count code test

--- src/test/java/CountMojoTest.java
+++ src/test/java/CountMojoTest.java
@@ -1,26 +1,41 @@
-import junit.framework.TestCase;
-import org.junit.jupiter.api.BeforeEach;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 
 /**
  * @author Zhaofeng Zhou
  * @date 7/10/2021 下午4:53
  */
-public class CountMojoTest extends TestCase {
+public class CountMojoTest extends AbstractMojoTestCase {
 
-    private CountMojo countMojo;
-
-
-    @BeforeEach
-    public void setUp() {
-        countMojo = new CountMojo();
-    }
+    private static final String PLUGIN_CONFIG = "src/test/java/count-test-plugin-config.xml";
 
     /**
      * @throws Exception
      */
     public void testMojoGoal() throws Exception {
-        countMojo.execute();
+        File testPom = getPom();
+        CountMojo mojo = (CountMojo) lookupMojo("countCode", testPom);
+
+        assertNotNull(mojo);
+
+
+        // generateTestFile()
+        // mojo.execute();
+    }
+
+
+    private File getPom() {
+        return new File(getBasedir(), PLUGIN_CONFIG);
+    }
+
+    private void generateTestFile(File directory, int num, String type) throws IOException {
+        for (int i = 0; i < num; i++) {
+            Files.createTempFile(directory.toPath(), "testFile" + i, type);
+        }
     }
 
 }
---
+++ src/test/java/testDirectory/java/JavaFile.java
@@ -0,0 +1,11 @@
+package testDirectory.java;
+
+/**
+ * @author Zhaofeng Zhou
+ * @date 8/10/2021 下午8:16
+ */
+public class JavaFile {
+    public static void main(String[] args) {
+        System.out.println("testFile!");
+    }
+}
